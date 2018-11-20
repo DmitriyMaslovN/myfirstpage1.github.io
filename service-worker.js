4ab82fc793d760320d574fbe6e6e6d2b04a54c09
DmitriyMaslovN <44729773+DmitriyMaslovN@users.noreply.github.com>
2018-11-20 18:58:12 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8939f91bebb64f45132d022013b1de9d.js"
+  "/myfirstpage1.github.io/precache-manifest.5eed37ad67d65f5a6f8bdabcfa1d9f3c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/myfirstpage1.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
