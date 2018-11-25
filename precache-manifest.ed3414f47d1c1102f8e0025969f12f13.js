18bb0d37221a3be2288dfe51a39cee011eed2ee1
DmitriyMaslovN <44729773+DmitriyMaslovN@users.noreply.github.com>
2018-11-25 20:10:39 +0200

    Updates

---
+++ precache-manifest.ed3414f47d1c1102f8e0025969f12f13.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdf93bd1bbcd6b8c6ee8",
+    "url": "/myfirstpage1.github.io/static/js/main.bdf93bd1.chunk.js"
+  },
+  {
+    "revision": "84c7e7c309ac8bd78327",
+    "url": "/myfirstpage1.github.io/static/js/1.84c7e7c3.chunk.js"
+  },
+  {
+    "revision": "4d058b132f843b53f144",
+    "url": "/myfirstpage1.github.io/static/js/runtime~main.4d058b13.js"
+  },
+  {
+    "revision": "1a42209a2ae9ddedfed28517e254e81f",
+    "url": "/myfirstpage1.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/myfirstpage1.github.io/precache-manifest.8595d49544662cbe86900742359ec831.js"
+  "/myfirstpage1.github.io/precache-manifest.ed3414f47d1c1102f8e0025969f12f13.js"
 );
 
 workbox.clientsClaim();
